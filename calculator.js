ffb19feb6e0db302f39966e553bd900451e4623c
Brian <briancawley93@gmail.com>
2022-04-02 08:45:57 +0100

    JS updates

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-
+let total = 0;
 
 
 const add = function(num1, num2) {
@@ -10,20 +10,16 @@ const add = function(num1, num2) {
     return num1 - num2;
       
   };
-  
-  const sum = function(arr) {
-    let total = arr.reduce(function(a, b){
-      return a + b;
-  }, 0);
-    return total;
+
+  function divide(num1, num2){
+    return num1 / num2;
   }
   
   
-  const multiply = function(arr) { 
-    let total = arr.reduce(function(a, b){
-      return a * b;
-  }, 1);
-    return total;
+  const multiply = function(num1, num2) { 
+    
+      return num1 * num2;
+  
   };
   
   const power = function(num1, num2) {
@@ -44,15 +40,90 @@ const add = function(num1, num2) {
     return base;
   };
   
+let operator =""
+
+  function operate(operator, num1, num2){
+    let newDisplay = 0;
+
+    switch(operator){
+      case "add":
+        newDisplay = add(num1,num2);
+        break;
+      case "sub":
+        newDisplay = subtract(num1,num2);
+        break;
+      case "divide":
+        newDisplay = divide(num1,num2);
+        break;
+      case "multiply":
+        newDisplay = multiply(num1,num2);
+        break;
+    }
+
+    return newDisplay    
+  }
+
+let display = document.getElementById("display")
+
+let appendMode = true;
+
+
+  //check if in write mode (append mode)
+    //if true then append to display string
+    //else start new string, append mode = true
+function addToDisplay(string){
+
+  if(appendMode){
+    display.innerHTML += ""+this.id;
+  }
+  else{
+    appendMode = true;
+    display.innerHTML = this.id;
+
+  }  
   
+}
+
+const updateTotal = function(){ 
+
+
+  let newTotal = operate(operator, Number(total), Number(display.innerHTML)) 
+  total = newTotal;
+  display.innerHTML = Number(total);
+  appendMode= false;
+  operator = this.id
+  console.log(this.id)
+}
+
+
+  //event listner on each number button
+    //concatenate clicked button to display string
+
+//Add event listener to each num button.
+for (i=0;i<10;i++){
+  let numBtn = document.getElementById(i)
+  let numValue = numBtn.innerHTML;
+  numBtn.addEventListener('click', addToDisplay);
+}
+
+  let plus = document.getElementById("add");
+  plus.addEventListener('click', updateTotal)
   
-  // Do not edit below this line
-  module.exports = {
-    add,
-    subtract,
-    sum,
-    multiply,
-    power,
-    factorial
-  };
+   let sub = document.getElementById("sub");
+   sub.addEventListener('click', updateTotal)
+
+
+
+//initial total and display = 0
+
+//on click num button
+  //check if in write mode (append mode)
+    //if true then append to display string
+    //else start new string, append mode = true
   
+
+//on click operate button (new fucntion)
+  //Update Display?
+  //appendMode = false;
+  //operator = this.id
+
